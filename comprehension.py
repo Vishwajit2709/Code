66f5eb9d9fa6fd3752ee11aed36df047d30ed40d
Vishwajit2709 <vishwajitpatil2728@gmail.com>
2024-03-24 20:08:04 +0530

    Addting business logics

---
+++ comprehension.py
@@ -0,0 +1,25 @@
+class CarModel: #classname
+    name = "BMW" #variable
+    gear = 5
+    def run(self):   #functions
+        print("Car gear is:- ", self.gear)
+        print("car name is:- ", self.name)
+    def gear1(self):
+        print("car gear is:- ", self.gear)
+car_obj = CarModel() #create the the object
+print(car_obj.name)
+car_obj.run() #with the help object, call the function.
+car_obj.gear1()
+
+class CarModel: #classname
+    name = "BMW" #variable
+    gear = 5
+    def run(self):   #functions
+        print("Car gear is:- ", self.gear)
+        print("car name is:- ", self.name)
+    def gear1(self):
+        print("car gear is:- ", self.gear)
+car_obj = CarModel() #create the the object
+print(car_obj.name)
+car_obj.run() #with the help object, call the function.
+car_obj.gear1()
